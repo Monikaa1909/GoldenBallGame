0901e2520fabdec203606ae91d15b7c39ade0eda
Monikaa1909 <monikaa1909@gmail.com>
2021-12-17 12:13:45 +0100

    present with extra gols

--- game.js
+++ game.js
@@ -35,22 +35,20 @@ function preload ()
     this.load.image('floorright2', 'assets/floorright2.png');
     this.load.image('floorright3', 'assets/floorright3.png');
 
-    this.load.spritesheet('star', 'assets/gol.png', { frameWidth: 24, frameHeight: 24});
-    this.load.image('bomb', 'assets/granat2.png');
+    this.load.spritesheet('ball', 'assets/gol.png', { frameWidth: 24, frameHeight: 24});
+    this.load.image('granat', 'assets/granat2.png');
     this.load.image('faul', 'assets/faul2.png');
 
     this.load.spritesheet('lewy', 'assets/lewy.png', { frameWidth: 96, frameHeight: 150 });
 
     this.load.image('speedoshee', 'assets/oshee.png');
     this.load.image('jumposhee', 'assets/oshee.png');
-    this.load.image('crate', 'assets/trophy.png');
+
+    this.load.image('trophy', 'assets/trophy.png');
     this.load.image('chatbox', 'assets/m_chatbox.png');
 //     this.load.image('tree', 'assets/Tree_2.png');
-    this.load.spritesheet('santa', 'assets/fifa.png', {frameWidth: 153.375, frameHeight: 150});
-//     this.load.spritesheet('santa', 'assets/fifaguys.png', {frameWidth: 153.375, frameHeight: 138});
-//     this.load.spritesheet('santa', 'assets/santa.png', {frameWidth: 934, frameHeight: 570});
-    this.load.spritesheet('ninja', 'assets/messi.png', {frameWidth: 112, frameHeight: 150});
-//     this.load.spritesheet('ninja', 'assets/ninja.png', {frameWidth: 377, frameHeight: 451});
+    this.load.spritesheet('fifa', 'assets/fifa.png', {frameWidth: 153.375, frameHeight: 150});
+    this.load.spritesheet('messi', 'assets/messi.png', {frameWidth: 112, frameHeight: 150});
 //     this.load.image('igloo', 'assets/Igloo.png');
 //     this.load.image('tree2', 'assets/Tree_1.png');
 //     this.load.image('snowman', 'assets/SnowMan.png');
@@ -58,7 +56,7 @@ function preload ()
 //     this.load.image('crystal', 'assets/Crystal.png');
     this.load.image('sign1', 'assets/Sign_1.png');
     this.load.image('sign2', 'assets/Sign_2.png');
-    this.load.image('treasure', 'assets/treasure.png');
+    this.load.image('present', 'assets/present.png');
     this.load.image('paper', 'assets/paper.png');
     this.load.image('lifecrystal', 'assets/lifecrystal.png');
     this.load.audio('terrariaday', 'assets/terrariaday.mp3');
@@ -66,17 +64,17 @@ function preload ()
 
 var platforms;
 var player;
-var stars;
+var balls;
 var score = 0;
-var santaSpeech = "";
+var fifaSpeech = "";
 var speedoshee;
 var speed = 200;
 var jumposhee;
 var jump = -800; //800
-var santa;
+var fifa;
 var dialogue=0;
-var crate;
-var crateState = 0;
+var trophy;
+var trophyState = 0;
 var chatbox;
 var visibleChat = 0;
 var tree;
@@ -84,10 +82,10 @@ var hitbox;
 var lastthrow = 0;
 var lastfaul = 0;
 var lasthit = 0;
-var ninja;
+var messi;
 var hearts = 3;
-var treasure;
-var treasurecoins;
+var present;
+var presentballs;
 var boughtLife=0;
 var lifecrystal;
 var sound;
@@ -97,6 +95,8 @@ function create ()
 {
 
     window.scene = this;
+
+    // DODANIE DŹWIĘKU (narazie strasznie irytuje więc wyłączyłam)
 //     sound = this.sound.add('terrariaday', {loop: true});
 //     sound.play();
 
@@ -169,12 +169,10 @@ function create ()
 //     this.add.image(180,385,'sign2').setScale(0.75);
 //     this.add.image(1700,575,'tree2').setScale(0.9);
 
-    lifecrystal = this.physics.add.staticGroup();
-    lifecrystal.create(1750, 675, 'lifecrystal').setScale(0.1).refreshBody();
-
-
 //     this.add.image(545,548,'stone').setScale(0.5);
 //     this.add.image(545,548,'stone').setScale(0.5);
+
+
 
 //     let crystal = this.add.image(650, 410, 'crystal').setScale(0.75);
 //     let crystal2 = this.add.image(190, 510, 'crystal').setScale(0.75);
@@ -189,13 +187,17 @@ function create ()
 //     let igloo = this.add.image(100,650,'igloo').setScale(0.5);
 //     igloo.flipX=true;
 
+    // USTAWIENIE LEWANDOWSKIEGO - GŁÓWNEGO GRACZA
     player = this.physics.add.sprite(300, 600, 'lewy').setScale(0.8).refreshBody();
 
+    // POSTAWIENIE SERDUSZKA DODAJĄCEGO ŻYCIE (które narazie nie działa)
+    lifecrystal = this.physics.add.staticGroup();
+    lifecrystal.create(1200, 475, 'lifecrystal').setScale(0.1).refreshBody();
+
+    // USTAWIENIE ZAKRESU KAMER I OGÓLNYCH SZCZEGÓŁÓW ŚWIATA
     this.cameras.main.setBounds(0,0,1500,760)
     this.cameras.main.startFollow(player, true);
     this.cameras.main.fadeIn(1000);
-
-
     this.physics.world.bounds.width = 1500
     this.physics.world.bounds.height = 800
     player.setBounce(0.2);
@@ -204,42 +206,37 @@ function create ()
     player.setDamping(true)
     player.setDrag(0.2)
 
-    this.physics.add.overlap(player, hitbox, ninjaThrow);
+    // WYWOŁANIE FUNKCJI RZUCAJĄCEGO MESSIEGO, WJEŹDŹAJĄCEGO Z FAULEM I DODAWANIA ŻYCIA (dodawanie życie nie działa, trza zrobić :( )
+    this.physics.add.overlap(player, hitbox, messiThrow);
     this.physics.add.overlap(player, hitbox, faulGo);
     this.physics.add.overlap(player, lifecrystal, buyLife);
 
+    // UŁOŻENIE DYMKU Z TEKSTEM LUDZI Z FIFY
     chatbox = this.physics.add.staticGroup();
     chatbox.create(225, 600, 'chatbox').setScale(0.065).refreshBody();
     chatbox.setVisible(false);
 
+    // UŁOŻENIE 2 OSHEE I TROFEUM
     speedoshee = this.physics.add.staticGroup();
     speedoshee.create(1335, 155, 'speedoshee');
 
-    crate = this.physics.add.staticGroup();
-    crate.create(800, 290, 'crate').setScale(0.24).refreshBody();
+    trophy = this.physics.add.staticGroup();
+    trophy.create(800, 290, 'trophy').setScale(0.24).refreshBody();
 
     jumposhee = this.physics.add.staticGroup();
     jumposhee.create(800, 555, 'jumposhee');
 
+    // USTAWIENIA GRAFIKI MESSIEGO, LUDZI Z FIFY I LEWANDOWSKIEGO
     this.anims.create({
-        key: 'ninja',
-        frames: this.anims.generateFrameNumbers('ninja', { start: 0, end: 9 }),
+        key: 'messi',
+        frames: this.anims.generateFrameNumbers('messi', { start: 0, end: 9 }),
         frameRate: 10,
         repeat: 0
     })
 
-
     this.anims.create({
-        key: 'coinanimation',
-        frames: this.anims.generateFrameNumbers('star', { start: 0, end: 6 }),
-        frameRate: 7,
-        repeat: -1
-    })
-
-    this.anims.create({
-        key: 'santaidle',
-//         frames: [ { key: 'santa', frame: 0 } ],
-        frames: this.anims.generateFrameNumbers('santa', { start: 0, end: 7 }),
+        key: 'fifaidle',
+        frames: this.anims.generateFrameNumbers('fifa', { start: 0, end: 7 }),
         frameRate: 7,
         repeat: -1
     })
@@ -261,79 +258,80 @@ function create ()
     this.physics.add.collider(player, platforms);
     cursors = this.input.keyboard.createCursorKeys();
 
-    treasure = this.physics.add.staticGroup();
-    treasure.create(1730, 315, 'treasure').setScale(0.2).refreshBody();
-    this.physics.add.overlap(player, treasure, collectTreasure);
+    // USTAWIENIE PREZENTU Z DODATKOWYMI PIŁKAMI
+    present = this.physics.add.staticGroup();
+    present.create(640, 545, 'present').setScale(0.2).refreshBody();
+    this.physics.add.overlap(player, present, collectPresent);
 
-    stars = this.physics.add.group({
-        key: 'star',
+    // SPADAJĄCE PIPŁKI
+    balls = this.physics.add.group({
+        key: 'ball',
         repeat: 20,
         setXY: { x: 12, y: 0, stepX: 70 }
     });
 
-
-    stars.children.iterate(function (child) {
+    balls.children.iterate(function (child) {
         child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.6));
         child.setBounceX(Phaser.Math.FloatBetween(0.4, 0.6));
         child.setVelocityX(Phaser.Math.FloatBetween(-100, 100));
 
-        child.anims.play('coinanimation', true);
+        child.anims.play('ballanimation', true);
         child.setCollideWorldBounds(true);
         child.setDamping(true)
         child.setDrag(0.5)
-
     });
 
-    treasurestars = this.physics.add.group({
-        key: 'star',
+    // PIŁKI PO ZDOBYCIU PREZENTU
+    presentballs = this.physics.add.group({
+        key: 'ball',
         repeat: 19,
-        setXY: { x: 1740, y: 100, stepX: 0 }
+        setXY: { x: 700, y: 100, stepX: 0 }
     });
 
-    treasurestars.children.iterate(function (child) {
+    presentballs.children.iterate(function (child) {
         child.setVisible(false);
-
-
     });
 
-    this.physics.add.collider(treasurestars, hitbox);
+    this.physics.add.collider(presentballs, hitbox);
 
-    treasurestars.setC
-    this.physics.add.overlap(player, treasurestars, collectStar);
+    presentballs.setC
+    this.physics.add.overlap(player, presentballs, collectBall);
 
-    santas = this.physics.add.group({
-        key: 'santa',
+    // USTAWIENIE LUDZI Z FIFY
+    fifas = this.physics.add.group({
+        key: 'fifa',
         setXY: { x: 60, y: 550, stepX: 100 },
     })
 
-
-    santas.children.iterate(function(child){
-        child.anims.play('santaidle', true);
+    fifas.children.iterate(function(child){
+        child.anims.play('fifaidle', true);
         child.setScale(0.8).refreshBody();
 
     })
 
-    ninja = this.physics.add.group({
-        key: 'ninja',
+    // USTAWIENIE MESSIEGO
+    messi = this.physics.add.group({
+        key: 'messi',
         setXY: { x: 650, y: 200, stepX: 70 },
     })
-    ninja.children.iterate(function(child){
+    messi.children.iterate(function(child){
         child.setScale(0.8).refreshBody();
     })
 
+    this.physics.add.collider(messi, platforms)
+    this.physics.add.collider(fifas, platforms)
+    this.physics.add.overlap(player, fifas, fifaColision)
+    this.physics.add.collider(balls, platforms);
+    this.physics.add.collider(presentballs, platforms);
+    this.physics.add.overlap(player, balls, collectBall);
 
-    this.physics.add.collider(ninja, platforms)
-    this.physics.add.collider(santas, platforms)
-    this.physics.add.overlap(player, santas, santaColision)
-    this.physics.add.collider(stars, platforms);
-    this.physics.add.collider(treasurestars, platforms);
-    this.physics.add.overlap(player, stars, collectStar);
-    santaSpeech = this.add.text(100, 583, '', { fontSize: '16px', fill: '#000000' });
-    bombs = this.physics.add.group();
+    fifaSpeech = this.add.text(100, 583, '', { fontSize: '16px', fill: '#000000' });
+
+    granats = this.physics.add.group();
     fauls = this.physics.add.group();
 
-    this.physics.add.collider(bombs, platforms);
-    this.physics.add.overlap(player, bombs, hitBomb);
+    this.physics.add.collider(granats, platforms);
+    this.physics.add.overlap(player, granats, hitGranat);
 
     this.physics.add.collider(fauls, platforms);
     this.physics.add.overlap(player, fauls, hitFaul);
@@ -341,25 +339,24 @@ function create ()
     this.physics.add.collider(speedoshee, platforms);
     this.physics.add.overlap(player, speedoshee, collectSpeedoshee);
 
-    this.physics.add.collider(speedoshee, crate);
-    this.physics.add.overlap(player, crate, collectCrate);
+    this.physics.add.collider(speedoshee, trophy);
+    this.physics.add.overlap(player, trophy, collectTrophy);
 
     this.physics.add.collider(jumposhee, platforms);
     this.physics.add.overlap(player, jumposhee, collectJumposhee);
 
-//     var bomb = bombs.create(600, 475, 'bomb');
-//     bomb.setBounce(1);
-//     bomb.setCollideWorldBounds(true);
-//     bomb.setVelocity(0, 200);
+//     var granat = granats.create(600, 475, 'granat');
+//     granat.setBounce(1);
+//     granat.setCollideWorldBounds(true);
+//     granat.setVelocity(0, 200);
 
 //     var faul = fauls.create(1500, 670, 'faul');
 //     faul.setScale(0.55).refreshBody();
 //     faul.setVelocity(-300, 0);
 
+    // WYNIKI
     this.add.image(90, 90, 'paper').setScrollFactor(0).setScale(0.3);
-
     this.text = this.add.text(32, 32).setScrollFactor(0).setFontSize(16).setColor('#000000');
-
 }
 
 function faulGo(player, hitbox){
@@ -371,28 +368,28 @@ function faulGo(player, hitbox){
                 faul.setScale(0.55).refreshBody();
                 faul.setVelocity(-300, 0);
           }, 500);
-
     }
 }
 
-function ninjaThrow(player, hitbox){
+function messiThrow(player, hitbox){
 
     if(Date.now() - lastthrow > 3000){
         lastthrow = Date.now();;
         setTimeout(function() {
-            var bomb = bombs.create(640, 260, 'bomb');
-            bomb.setVelocity(Phaser.Math.FloatBetween(-350, -250), -200);
-            bomb.setBounce(1);
+            var granat = granats.create(640, 260, 'granat');
+            granat.setVelocity(Phaser.Math.FloatBetween(-350, -250), -200);
+            granat.setBounce(1);
           }, 200);
 
-        ninja.children.iterate(function(child){
-            var ninjaanim = child;
-            child.anims.play('ninja', true);
+        messi.children.iterate(function(child){
+            var messianim = child;
+            child.anims.play('messi', true);
             child.setScale(0.8).refreshBody();
         })
     }
 }
 
+// FUNKCJA Z DODAWANIEM ŻYCIA DO OGARNIĘCIA
 function buyLife(player, lifecrystal){
     if(score>99){
         if(Date.now() - boughtLife > 1000){
@@ -403,29 +400,29 @@ function buyLife(player, lifecrystal){
     }
 }
 
-function santaColision(player, santas){
+function fifaColision(player, fifas){
 
-    if(crateState == 0){
+    if(trophyState == 0){
         if(dialogue==0){
             visibleChat = 1;
-            santaSpeech.setText('Chcesz zdobyć złotą piłkę? \n Nie tak prędko mój drogi');
+            fifaSpeech.setText('Chcesz zdobyć złotą piłkę? \n Nie tak prędko mój drogi');
             dialogue = 1;
         }
         else if(dialogue==1){
         setTimeout(function() {
-            santaSpeech.setText('Zdobądź jak najwięcej goli! \n  Ale to nie wszystko...');
+            fifaSpeech.setText('Zdobądź jak najwięcej goli! \n  Ale to nie wszystko...');
             dialogue = 2;
           }, 2000);
         }
         else if(dialogue==2){
             setTimeout(function() {
-                santaSpeech.setText(' Jakiś puchar za osiągnięcia\ndrużynowe też by się przydał');
+                fifaSpeech.setText(' Jakiś puchar za osiągnięcia\ndrużynowe też by się przydał');
                 dialogue = 3;
             }, 2000);
         }
         else if(dialogue == 3){
             setTimeout(function() {
-              santaSpeech.setText('');
+              fifaSpeech.setText('');
               dialogue = 4;
               visibleChat = 0;
             }, 2000);
@@ -433,19 +430,19 @@ function santaColision(player, santas){
     } else {
         if(dialogue == 0){
             visibleChat = 1;
-            santaSpeech.setText('Odnalazła się!');
+            fifaSpeech.setText('Odnalazła się!');
             dialogue = 1;
         }
         else if (dialogue == 1){
             setTimeout(function() {
-                santaSpeech.setText('Oto obiecana nagroda');
+                fifaSpeech.setText('Oto obiecana nagroda');
                 dialogue = 2;
                 score += 2;
               }, 2000);
         }
         else if (dialogue == 2){
             setTimeout(function() {
-                santaSpeech.setText('');
+                fifaSpeech.setText('');
                 dialogue = 3;
                 visibleChat = 0;
             }, 2000);
@@ -453,15 +450,15 @@ function santaColision(player, santas){
     }
 }
 
-function collectStar (player, star)
+function collectBall (player, ball)
 {
-    star.disableBody(true, true);
+    ball.disableBody(true, true);
     dialogue = 0;
     score += 10;
 
-    if (stars.countActive(true) === 0)
+    if (balls.countActive(true) === 0)
     {
-        stars.children.iterate(function (child) {
+        balls.children.iterate(function (child) {
 
             child.enableBody(true, child.x, 0, true, true);
 
@@ -469,23 +466,23 @@ function collectStar (player, star)
 
         var x = (player.x < 900) ? Phaser.Math.Between(900,1500) : Phaser.Math.Between(0, 900);
 
-        var bomb = bombs.create(x, 16, 'bomb');
-        bomb.setBounce(1);
-        bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(Phaser.Math.Between(-150, 150), 150);
+        var granat = granats.create(x, 16, 'granat');
+        granat.setBounce(1);
+        granat.setCollideWorldBounds(true);
+        granat.setVelocity(Phaser.Math.Between(-150, 150), 150);
 
     }
 }
 
-function collectTreasure(player, treasure){
-    treasure.disableBody(true, true);
-    treasurestars.children.iterate(function (child) {
+function collectPresent(player, present){
+    present.disableBody(true, true);
+    presentballs.children.iterate(function (child) {
         child.setVisible(true)
         child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.6));
         child.setBounceX(Phaser.Math.FloatBetween(0.4, 0.6));
         child.setVelocityX(Phaser.Math.FloatBetween(-100, 100));
         child.setVelocityY(Phaser.Math.FloatBetween(-1600, -1400));
-        child.anims.play('coinanimation', true);
+        child.anims.play('ballanimation', true);
         child.setCollideWorldBounds(true);
         child.setDamping(true)
         child.setDrag(0.5)
@@ -493,9 +490,10 @@ function collectTreasure(player, treasure){
 
 }
 
-function collectCrate(player, crate){
-    crate.disableBody(true, true)
-    crateState = 1;
+// ZDOBYCIE PUCHARU
+function collectTrophy(player, trophy){
+    trophy.disableBody(true, true)
+    trophyState = 1;
     dialogue = 0;
 }
 
@@ -511,7 +509,7 @@ function collectJumposhee(player, speedoshee){
     jump-=200;
 }
 
-function hitBomb (player, bomb)
+function hitGranat (player, granat)
 {
     player.setTint(0xff0000);
     if(hearts == 0){
